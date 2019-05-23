98ad5840ffad3b304d54135b86bfa143bbc131ef
Andrew M <beeengine@gmail.com>
2019-05-23 23:34:47 +0300

    engineers & specialties added

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sinatra/namespace'
 require 'sequel'
 require 'pg'
 
@@ -16,6 +18,73 @@ helpers do
   end
 end
 
+namespace '/admin' do
+  set :views, settings.root + '/views/admin'
+  get '/?' do
+    erb :home, locals: { articles: DB[:articles], engineers: DB[:engineers], specialties: DB[:specialties] }
+  end
+
+  get '/article' do
+    locals = if params[:dup]
+               { article: { body: DB[:articles].where(id: params[:id]).first[:body], lang: params[:lang].to_i }, error: nil, articles: DB[:articles].where(parent_id: nil) }
+             else
+               { article: DB[:articles].where(id: params[:id]).first || {body: 'Текст сюди', lang: 1}, error: nil, articles: DB[:articles].where(parent_id: nil) }
+             end
+    erb :article, locals: locals
+  end
+
+  post '/article' do
+    begin
+      if params[:id].empty?
+        DB[:articles].insert(clear_params(params[:article]))
+      else
+        DB[:articles].where(id: params[:id]).update(clear_params(params[:article]))
+      end
+      redirect '/admin'
+    rescue Sequel::Error => e
+      erb :article, locals: { error: e, article: params[:article], articles: DB[:articles] }
+    end
+  end
+
+  get '/specialty' do
+    locals = { specialty: DB[:specialties].where(id: params[:id]).first || {}, error: nil }
+    erb :specialty, locals: locals
+  end
+
+  post '/specialty' do
+    begin
+      if params[:id].empty?
+        DB[:specialties].insert(clear_params(params[:specialty]))
+      else
+        DB[:specialties].where(id: params[:id]).update(clear_params(params[:specialty]))
+      end
+      redirect '/admin'
+    rescue Sequel::Error => e
+      erb :specialty, locals: { error: e, specialty: params[:specialty] }
+    end
+  end
+
+  get '/engineer' do
+    engineer = DB[:engineers].left_join(DB[:engineer_translations]).where('engineers.id = ?', params[:id])
+    locals = { engineer: engineer || {}, error: nil, specialties: DB[:specialties] }
+    erb :engineer, locals: locals
+  end
+
+  post '/engineer' do
+    begin
+      if params[:id].empty?
+        DB[:engineers].insert(clear_params(params[:engineer]))
+      else
+        DB[:engineers].where(id: params[:id]).update(clear_params(params[:engineer]))
+      end
+      redirect '/admin'
+    rescue Sequel::Error => e
+      erb :engineer, locals: { error: e, engineer: params[:engineer], specialties: DB[:specialties] }
+    end
+  end
+end
+
+
 get '/' do
   # slim :home, layout: false, locals: { l: settings.ua }
   erb :home, locals: { articles: DB[:articles] }
@@ -23,31 +92,6 @@ end
 
 get '/team' do
   erb :team
-end
-
-get '/admin' do
-  erb :admin, locals: { articles: DB[:articles] }
-end
-get '/article' do
-  locals = if params[:dup]
-             { article: { body: DB[:articles].where(id: params[:id]).first[:body], lang: params[:lang].to_i }, error: nil, articles: DB[:articles].where(parent_id: nil) }
-           else
-             { article: DB[:articles].where(id: params[:id]).first || {body: 'Текст сюди', lang: 1}, error: nil, articles: DB[:articles].where(parent_id: nil) }
-           end
-  erb :edit, locals: locals
-end
-
-post '/article' do
-  begin
-    if params[:id].empty?
-      DB[:articles].insert(clear_params(params[:article]))
-    else
-      DB[:articles].where(id: params[:id]).update(clear_params(params[:article]))
-    end
-    redirect '/admin'
-  rescue Sequel::Error => e
-    erb :edit, locals: {error: e, article: params[:article], articles: DB[:articles] }
-  end
 end
 
 get '/:slug' do
--- lib/add_db.rb
+++ lib/add_db.rb
@@ -13,3 +13,62 @@ DB.create_table :articles do
   Integer :lang, null: false
   foreign_key :parent_id
 end
+
+DB.create_table :specialties do
+  primary_key :id
+  String :image
+  String :name_en, unique: true, null: false
+  String :slug_en, unique: true, null: false
+  String :name_ua, unique: true, null: false
+  String :slug_ua, unique: true, null: false
+  String :name_ru, unique: true, null: false
+  String :slug_ru, unique: true, null: false
+end
+
+DB.create_table :engineers do
+  primary_key :id
+  foreign_key :specialty_id, null: false
+  String :image
+end
+
+DB.create_table :engineer_translations do
+  primary_key :id
+  foreign_key :engineer_id, null: false
+  Integer :lang, null: false
+  String :slug, null: false
+  String :name, null: false
+  String :specialty, null: false
+  String :resume, null: false
+  String :desc, text: true
+end
+
+#seed
+
+DB[:articles].insert(title: 'Головна', body: '<h2>Це головна сторінка, вітання!</h2>', lang: 1)
+DB[:articles].insert(title: 'Main', slug: 'main', body: '<h2>Here is the main page, congrats!</h2>', lang: 0)
+DB[:articles].insert(title: 'Главная', slug: 'главная', body: '<h2>Это заглавная страница, мое почтение, уважаемый господин!</h2>', lang: 2)
+
+gold_uk = DB[:articles].insert(title: 'Золото', slug: 'золото', body: '<h2>Текст про золото</h2>', lang: 1)
+DB[:articles].insert(title: 'Розсипне Золото', slug: 'розсипне-золото', body: '<h2>Текст про розсипне золото</h2>', lang: 1, parent_id: gold_uk)
+
+gold_ru = DB[:articles].insert(title: 'Золото', slug: 'золото-ru', body: '<h2>Текст про золото</h2>', lang: 2)
+DB[:articles].insert(title: 'Роcсыпное Золото', slug: 'россыпное-золото', body: '<h2>Текст о россыпном золоте</h2>', lang: 2, parent_id: gold_ru)
+
+geologists = DB[:specialties].insert(image: 'https://ucarecdn.com/6d6970b9-402c-4597-8a08-ca02be8affb7/9774608.jpg',
+                                     name_en: 'Geologists', name_ua: 'Геологи', name_ru: 'Геологи',
+                                     slug_en: 'geologists', slug_ua: 'геологи', slug_ru: 'геологи')
+drillers = DB[:specialties].insert(image: 'https://ucarecdn.com/6d6970b9-402c-4597-8a08-ca02be8affb7/9774608.jpg',
+                                     name_en: 'Drillers', name_ua: 'Буровики', name_ru: 'Буровики',
+                                     slug_en: 'drillers', slug_ua: 'буровики', slug_ru: 'буровики')
+technologists = DB[:specialties].insert(image: 'https://ucarecdn.com/6d6970b9-402c-4597-8a08-ca02be8affb7/9774608.jpg',
+                                     name_en: 'Technologists', name_ua: 'Технологи', name_ru: 'Технологи',
+                                     slug_en: 'technologists', slug_ua: 'технологи', slug_ru: 'технологи')
+geophysics = DB[:specialties].insert(image: 'https://ucarecdn.com/6d6970b9-402c-4597-8a08-ca02be8affb7/9774608.jpg',
+                                     name_en: 'Geophysics', name_ua: 'Геофізики', name_ru: 'Геофизики',
+                                     slug_en: 'geophysics', slug_ua: 'геофізики', slug_ru: 'геофизики')
+miners = DB[:specialties].insert(image: 'https://ucarecdn.com/6d6970b9-402c-4597-8a08-ca02be8affb7/9774608.jpg',
+                                     name_en: 'Miners', name_ua: 'Гірняки', name_ru: 'Горняки',
+                                     slug_en: 'miners', slug_ua: 'гірняки', slug_ru: 'горняки')
+surveyors = DB[:specialties].insert(image: 'https://ucarecdn.com/6d6970b9-402c-4597-8a08-ca02be8affb7/9774608.jpg',
+                                     name_en: 'Surveyors', name_ua: 'Геодезисти', name_ru: 'Геодезисты',
+                                     slug_en: 'surveyors', slug_ua: 'геодезисти', slug_ru: 'геодезисты')
